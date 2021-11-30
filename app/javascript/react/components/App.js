0cb2a80d0d8b8ea4baceb01c405e35ae280d3a6d
Alex Ballard <alex.c.ballard@gmail.com>
2021-11-30 14:57:58 -0500

    Added Item Show Page. Added Links on Index Page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import ItemsIndex from './ItemsIndex'
+import ItemShow from './ItemShow'
 
 export const App = (props) => {
   return (
     <BrowserRouter>
       <Switch>
 				<Route exact path="/" component={ItemsIndex} />
+        <Route exact path="/items/:id" component={ItemShow} />
 			</Switch>
     </BrowserRouter>
   )
